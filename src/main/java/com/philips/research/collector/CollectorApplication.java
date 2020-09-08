ed7d38e3840ce44a10204b4ab2bde80f252ae05f
Timo van de Put <timo.van.de.put@philips.com>
2020-09-08 15:12:41 +0200

    Create initial Spring Boot application

---
+++ src/main/java/com/philips/research/collector/CollectorApplication.java
@@ -0,0 +1,13 @@
+package com.philips.research.collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/philips/research/collector/CollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.philips.research.collector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
