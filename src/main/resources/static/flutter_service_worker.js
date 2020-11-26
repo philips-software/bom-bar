632d3eeace9ca5200514f76bbbbd93310c19c2fa
Timo van de Put <timo.van.de.put@philips.com>
2020-11-26 09:50:45 +0100

    Update UI

--- src/main/resources/static/flutter_service_worker.js
+++ src/main/resources/static/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "423072ad2cd7894e1d8008f9c41f81b8",
-"index.html": "d30c702fd314e4f6f94135a9510ad852",
-"/": "d30c702fd314e4f6f94135a9510ad852",
-"main.dart.js": "7daa7639a36de512f798146569b29ccd",
+"index.html": "7f31ac7122a3a9f8cd3d8858042e7162",
+"/": "7f31ac7122a3a9f8cd3d8858042e7162",
+"main.dart.js": "3c1c3c369f669dd1403ae586580e5415",
 "favicon.png": "4708e9d867b33c1cdc52102923f1ae96",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
